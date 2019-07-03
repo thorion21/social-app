7e97d7f20ee1262587ebb829c63bcb8e8ab828fd
ClaudiaP <claudia.prd23@gmail.com>
2019-07-03 11:27:37 +0300

    Add UserProfile/Country to Admin interface refs #9

--- socialapp/core/admin.py
+++ socialapp/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, UserProfile, Country
 # Register your models here.
 
 
@@ -15,5 +15,42 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ['created_by', 'text', 'post']
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('get_username','get_firstname', 'get_lastname', 'get_country',)
+    search_fields = ['user__username', 'user__first_name', 'user__last_name', 'country__name']
+    list_filter = ('user__is_superuser', 'country__name')
+
+    def get_username(self, obj):
+        return obj.user.username
+
+    get_username.short_description = 'Username'
+    get_username.admin_order_field = 'user__username'
+
+    def get_country(self, obj):
+        return obj.country.name
+
+    get_country.short_description = 'Country'
+    get_country.admin_order_field = 'country__name'
+
+    def get_firstname(self, obj):
+        return obj.user.first_name
+
+    get_firstname.short_description = 'FirstName'
+    get_firstname.admin_order_field = 'user__first_name'
+
+    def get_lastname(self, obj):
+        return obj.user.last_name
+
+    get_lastname.short_description = 'LastName'
+    get_firstname.admin_order_field = 'user__last_name'
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name')
+    search_fields = ['code', 'name']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Country, CountryAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
