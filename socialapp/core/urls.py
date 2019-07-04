47f1c3e20b2b37ae7156c8022fafc49a8035091f
ClaudiaP <claudia.prd23@gmail.com>
2019-07-04 17:37:36 +0300

    Do register page and create new user #refs 24

--- socialapp/core/urls.py
+++ socialapp/core/urls.py
@@ -6,5 +6,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile/<int:user_id>/',views.user_profile_page, name='user_profile_page_view'),
-    path('posts/', views.posts_page, name='posts')
+    path('posts/', views.posts_page, name='posts'),
+    path('register/', views.register_page, name='register')
+
 ]
